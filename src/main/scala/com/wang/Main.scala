06cd832eb9bcc09dded1cdedd78972494ffa1de0
Meng Wang <wangmeng@gmail.com>
2012-10-22 12:48:55 -0700

    cli parser added

--- src/main/scala/com/wang/Main.scala
+++ src/main/scala/com/wang/Main.scala
@@ -10,20 +10,39 @@ import com.twitter.finagle.http.Http
 
 import com.twitter.querulous.evaluator.QueryEvaluator
 
+import scopt.immutable.OptionParser
+
+case class Config (httpport:Int=65000, host:String="localhost", dbport:Int = 3306,
+                   db:String="non-exist", dbuser:String="nobody", dbpwd:String="") 
+
 
 object Main {
   def main(args: Array[String]) {
 
-    val respond = new VoucherHttpRespond(new VoucherService(QueryEvaluator(
-      "localhost:3306", "voucherdb", "voucher_admin", "123", Map[String, String](), "jdbc:mysql")))
-    val handleException = new HandleExceptions
+    val parser = new OptionParser[Config]("voucher server", "alpha") {
+      def options = Seq(
+        intOpt("port", "port of http service") { case(p, c) => c.copy(httpport=p) },
+        opt("dbhost", "mysql host") { case(h, c) => c.copy(host=h) },
+        intOpt("dbport", "mysql port") { case(p, c) => c.copy(dbport=p) },
+        opt("db", "db name") { case(d, c) => c.copy(db=d) },
+        opt("dbuser", "db user, be sure to have r/w privilege") { case(u, c) => c.copy(dbuser=u) },
+        opt("dbpwd", "password") { case(p, c) => c.copy(dbpwd=p) }
+      )
+    }
 
-    val server = ServerBuilder()
-      .codec(Http())
-      .bindTo(new InetSocketAddress(8080))
-      .name("httpserver")
-      .build(handleException andThen respond)
+    parser parse(args, Config()) map { config =>
+      val respond = new VoucherHttpRespond(new VoucherService(QueryEvaluator(
+        "%s:%d" format(config.host, config.dbport), config.db, config.dbuser, config.dbpwd, Map[String, String](), "jdbc:mysql")))
+      val handleException = new HandleExceptions
 
+      val server = ServerBuilder()
+        .codec(Http())
+        .bindTo(new InetSocketAddress(config.httpport))
+        .name("httpserver")
+        .build(handleException andThen respond)
+    } getOrElse {
+      parser.usage
+    }
 
   }
 }
