9b1f970dc516a5a894805a82fe1618aad8189e10
Meng Wang <wangmeng@gmail.com>
2013-03-10 17:57:19 -0700

    allow to specify jdbc driver

--- src/main/scala/com/wang/Main.scala
+++ src/main/scala/com/wang/Main.scala
@@ -9,13 +9,13 @@ import com.twitter.conversions.time._
 import com.twitter.finagle.builder.ServerBuilder
 import com.twitter.finagle.http.Http
 
-import com.twitter.querulous.config.AsyncQueryEvaluator
-import com.twitter.querulous.config.ApachePoolingDatabase
+import com.twitter.querulous.config.{ApachePoolingDatabase, AsyncQueryEvaluator, Connection}
 
 import scopt.immutable.OptionParser
 
 case class Config (httpport:Int=65000, host:String="localhost", dbport:Int = 3306,
                    db:String="non-exist", dbuser:String="nobody", dbpwd:String="",
+                   dbdriver: Option[String] = None,
                    qrsize:Int=160) 
 
 
@@ -30,6 +30,7 @@ object Main {
         opt("db", "db name") { case(d, c) => c.copy(db=d) },
         opt("dbuser", "db user, be sure to have r/w privilege") { case(u, c) => c.copy(dbuser=u) },
         opt("dbpwd", "password") { case(p, c) => c.copy(dbpwd=p) },
+        opt("dbdriver", "jdbc driver") { case(d, c) => c.copy(dbdriver = Some(d)) },
         intOpt("qrsize" ,"size of the qr code png") { case (s, c) => c.copy(qrsize=s) }
       )
     }
@@ -49,9 +50,20 @@ object Main {
     }
 
 
+
     parser parse(args, Config()) map { config =>
-      val queryEvaluator = queryConfig()("%s:%d" format(config.host, config.dbport),
-                                         config.db, config.dbuser, config.dbpwd)
+      val connection = new Connection {
+        val hostnames = Seq("%s:%d" format(config.host, config.dbport))
+        val database = config.db
+        val username = config.dbuser
+        val password = config.dbpwd
+        urlOptions = Map(
+          "ssl" -> "true",
+          "sslfactory" -> "org.postgresql.ssl.NonValidatingFactory"
+        )
+        driverName = config.dbdriver.getOrElse("jdbc:mysql")
+      }
+      val queryEvaluator = queryConfig()(connection)
       val respond = new VoucherHttpRespond(new VoucherService(queryEvaluator, config.qrsize))
       val handleException = new HandleExceptions
 
